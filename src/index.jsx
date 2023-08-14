292761dccdaee553f4fd18437bdb6e9dcb89b62b
avcdsld <avcdsld@gmail.com>
2023-08-14 10:16:33 +0900

    upgrade packages

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,10 @@
+import { Buffer } from 'buffer';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
+
+window.Buffer = window.Buffer || Buffer;
 
 ReactDOM.render(
   <React.StrictMode>
